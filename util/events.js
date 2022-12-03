839cb03f32e7ce157c2316d6b87cf81237bc453c
Erim Vurucu <55758293+ErimV@users.noreply.github.com>
2022-12-03 16:48:33 +0300

    Delete events.js

--- util/events.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export async function getEvents(day,month){
-
-    const response = await axios.get("https://byabbe.se/on-this-day/1/23/events.json");
-    
-    return response.data.events;
-}

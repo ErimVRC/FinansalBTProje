93ad6a7c9921b708f45e1c3c1f35047addae2a5e
emrepektemek <45429524+emrepektemek@users.noreply.github.com>
2022-12-09 21:18:33 +0300

    Add files via upload

---
+++ Source Code/App.js
@@ -0,0 +1,105 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList,Image } from 'react-native';
+
+import FindEventsScreen from './screens/FindEventsScreen';
+import FindBirthsScreen from './screens/FindBirthsScreen';
+import FindDeathsScreen from './screens/FindDeathsScreen';
+import EventsScreen from './screens/EventsScreen';
+import BirthsScreen from './screens/BirthsScreen';
+import DeathsScreen from './screens/DeathsScreen';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { GlobalStyles } from './components/constants/styles';
+
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+
+function BulEkranlari(){
+  return(
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarInactiveBackgroundColor: GlobalStyles.colors.tabBarBorder,
+        tabBarLabelStyle:{
+          color: GlobalStyles.colors.tabBarLabel,
+          fontSize: 15,
+          fontFamily: 'sans-serif-medium',
+          fontWeight: 'bold'
+        },
+        tabBarStyle:{
+          height:53
+        },
+        tabBarActiveBackgroundColor: GlobalStyles.colors.bottomActiveTabBar,
+        tabBarIcon: ({focused})=>{
+          return(
+            <Image 
+              style={styles.bottomTabImage}
+              source={{
+                //uri: 'https://iconarchive.com/download/i31640/sykonist/popular-sites/Wikipedia.ico'
+                uri: 'https://cdn-icons-png.flaticon.com/512/5968/5968992.png'
+                
+              }}
+            />
+          );
+        },
+      }}
+    >
+    <BottomTabs.Screen 
+      name="OlaylariGetirEkrani"
+      component={FindEventsScreen}
+      options={{title: 'Find Events'}}
+    />
+    <BottomTabs.Screen 
+      name="DogumlariGetirEkrani"
+      component={FindBirthsScreen}
+      options={{title: 'Find Birthdays'}}
+    />
+    <BottomTabs.Screen 
+      name="OlumleriGetirEkrani"
+      component={FindDeathsScreen}
+      options={{title: 'Find Deaths'}}
+    />   
+    </BottomTabs.Navigator>
+  );
+}
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+     <Stack.Navigator
+      screenOptions={{headerShown: false, headerBackVisible:true}}
+     >
+      <Stack.Screen 
+        name="BulEkranlari"
+        component={BulEkranlari}
+      />
+      <Stack.Screen 
+        name="OlaylarEkrani"
+        component={EventsScreen}
+      />
+      <Stack.Screen 
+        name="DogumlarEkrani"
+        component={BirthsScreen}
+      />
+      <Stack.Screen 
+        name="OlumlerEkrani"
+        component={DeathsScreen}
+      />
+     </Stack.Navigator>
+    </NavigationContainer>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+    bottomTabImage:{
+        height: 30,
+        width: 30
+    }
+});
---
+++ Source Code/screens/FindBirthsScreen.js
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import { StyleSheet, View, Image, Text, TextInput, Alert } from "react-native";
+import { GlobalStyles } from "../components/constants/styles";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+import Button from "../components/UI/Button";
+
+function FindBirthsScreen({navigation}){
+    
+    const [day,setDay] = useState(0);
+    const [month,setMonth] = useState(0);
+
+    const [gunHata,setGunHata] = useState(1);
+    const [ayHata,setAyHata] = useState(1);
+
+    function saveDay(day){
+        setDay(day);
+        if(day>0 && day<=31){
+            setGunHata(0);  
+        }
+        else{
+            setGunHata(1);    
+        }
+    }
+    function saveMonth(month){
+        setMonth(month);
+        if(month>0 && month<=12){
+            setAyHata(0);
+        }
+        else{
+            setAyHata(1);
+        }
+    }
+
+    function searchBirths(){
+        if(gunHata == 0 && ayHata == 0){
+            navigation.navigate('DogumlarEkrani',{day: day, month: month});
+        }
+        else{
+            Alert.alert(
+                "Wrong Input",
+                "Please check the day and month value",
+                [
+                    { text: "OK", onPress: () => {} }
+                ]
+            );
+        }
+    }
+    
+    return(
+        <LinearGradient 
+            style={styles.mainContainer}
+            colors={['#d2f8fff2','#9df2ff','#93d9f8','#4db7f5']}
+        >
+            <Text style={styles.text}>Wikipedia Find Births</Text>
+            <Image
+                style={styles.image}
+                source={{
+                    uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Wikipedia_svg_logo.svg/2048px-Wikipedia_svg_logo.svg.png'
+                }}
+            />
+            <View style={styles.inputContainer}>
+                <TextInput 
+                    style={styles.input}
+                    keyboardType={"number-pad"}
+                    maxLength={2}
+                    placeholder={"Day"}
+                    placeholderTextColor={GlobalStyles.colors.dark}
+                    textAlign={'center'}
+                    value={day}
+                    onChangeText={saveDay}              
+                />
+                <TextInput 
+                    style={styles.input}
+                    keyboardType={"number-pad"}
+                    maxLength={2}
+                    placeholder={"Month"}
+                    placeholderTextColor={GlobalStyles.colors.dark}
+                    textAlign={'center'}
+                    value={month}
+                    onChangeText={saveMonth}    
+                />
+            </View>
+            <Button onPress={searchBirths}>Search</Button>
+        </LinearGradient>
+    );
+}
+
+export default FindBirthsScreen;
+
+
+const styles = StyleSheet.create({
+    mainContainer:{
+        flex: 1,
+        padding: 40,
+        alignItems: 'center'
+    },
+    image:{
+        height: 300,
+        width: 300
+    },
+    text:{
+        textAlign: 'center',
+        marginTop: 20,
+        fontSize: 25,
+        fontWeight: '450',
+        letterSpacing: 3,
+        color: GlobalStyles.colors.dark,
+        fontFamily: 'serif'
+    },
+    inputContainer:{
+        flexDirection: 'row'
+    },
+    input:{
+        height: 40,
+        width: '50%',
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: GlobalStyles.colors.dark,
+        color: GlobalStyles.colors.dark,
+        fontSize: 17,
+        marginBottom: 30
+    }
+});

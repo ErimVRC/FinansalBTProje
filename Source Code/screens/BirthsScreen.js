a0d67c09f88b3b1b07b00182bf6bf14ad801c35b
emrepektemek <45429524+emrepektemek@users.noreply.github.com>
2022-12-15 11:42:55 +0300

    Update BirthsScreen.js

--- Source Code/screens/BirthsScreen.js
+++ Source Code/screens/BirthsScreen.js
@@ -126,7 +126,7 @@ const styles = StyleSheet.create({
     borderTopColor:GlobalStyles.colors.dark,
     borderRadius: 60,
     padding: 10,
-    marginRight:40
+    marginRight: 50
   },
   yilContainer:{
     flex: 1,
